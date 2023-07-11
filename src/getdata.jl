f45ae798a0fb87371654ce50a3da52e7969835ad
Pitipat-PC <pitipatw@mit.edu>
2023-07-11 00:58:54 -0400

    format files

--- src/getdata.jl
+++ src/getdata.jl
@@ -10,11 +10,11 @@ using HTTP, JSON, DataFrames
 include("key.jl")
 
 function get_data(cate::String, page::Int64
-    ;token::String = token, mode::Int64 = 0)
+    ; token::String=token, mode::Int64=0)
     if mode == 0
-        res = HTTP.request( "GET",
-        "https://buildingtransparency.org/api/$cate/?page_number=$page", #materials?page_number=2",
-        ["Authorization" => "Bearer "*"$token"]
+        res = HTTP.request("GET",
+            "https://buildingtransparency.org/api/$cate/?page_number=$page", #materials?page_number=2",
+            ["Authorization" => "Bearer " * "$token"]
         )
     else
         println("Mode not found")
@@ -32,42 +32,43 @@ df = DataFrame(msg)
 ##########
 
 function scrapeit()
-cate = "materials"
-page = 1 #start from page 1
-total_pages = 0
-filepath = joinpath(@__DIR__,"rawdata/")
+    cate = "materials"
+    page = 1 #start from page 1
+    total_pages = 0
+    filepath = joinpath(@__DIR__, "rawdata/")
 
 
-#initialize variables
-page = 1 ; total_pages = 0;
-@time while page != total_pages
-    
-    res = get_data(cate, page)
-    if page == 1 #first run
-        total_pages = parse(Int64,(res["X-Total-Pages"]))
-        println("Scraping process started")
-        println("There are :", total_pages, " pages")
+    #initialize variables
+    page = 1
+    total_pages = 0
+    @time while page != total_pages
+
+        res = get_data(cate, page)
+        if page == 1 #first run
+            total_pages = parse(Int64, (res["X-Total-Pages"]))
+            println("Scraping process started")
+            println("There are :", total_pages, " pages")
+        end
+
+        response_text = String(res.body)
+
+        #get number to 0000 format for filename
+        spg = "0"^(4 - length(string(page))) * string(page)
+        filename = "pg" * string(spg) * ".json"
+
+        msg = JSON.parse(response_text)
+        #write a to a json file
+        println(filepath * filename)
+        open(filepath * filename, "w") do f
+            JSON.print(f, msg)
+        end
+        println("Page ", page, " done")
+        page += 1
+        res = nothing #clear the memory
+        # if page == 2 
+        #     println("Test Run Ended")
+        #     break
+        # end
     end
-
-    response_text = String(res.body)
-
-    #get number to 0000 format for filename
-    spg = "0"^(4-length(string(page)))*string(page)
-    filename = "pg" * string(spg) * ".json"
-
-    msg = JSON.parse(response_text)
-    #write a to a json file
-    println(filepath*filename)
-    open(filepath*filename, "w") do f
-        JSON.print(f, msg)
-    end
-    println("Page ", page, " done")
-    page += 1
-    res = nothing #clear the memory
-    # if page == 2 
-    #     println("Test Run Ended")
-    #     break
-    # end
-end
 end
 scrapeit()
